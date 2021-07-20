a77b84652d0b7f7372b6b1394dab199245fed8a6
ashwith31 <ashwitash@gmail.com>
2021-07-20 12:14:58 +0530

    modified:Created Dictionary and added values

--- ArithmeticSorting.sh
+++ ArithmeticSorting.sh
@@ -1,4 +1,5 @@
 #! /bin/bash -x
+declare -A AA
 
 PartTimeHour=8
 echo "Welcome to Arithmetic Sorting"
@@ -9,9 +10,14 @@ read c
 
 p=$(( a + b * c ))
 echo "(a+b*c)==$p"
+AA[1]=$p
 q=$(( a*b+c ))
 echo "(a*b+c)==$q"
+AA[2]=$q
 r=$(( c+a/b ))
 echo "(c+a/b)==$r"
+AA[3]=$r
 s=$(( a%b+c ))
 echo "(a%b+c)==$s"
+AA[4]=$s
+echo "The values in dictionary are: ${AA[*]}"
